dfe33ad848ce832f1f02c3e5e3f5129e091e1d8b
alessandro <finialessandro90@gmail.com>
2021-03-10 18:07:11 +0100

    pagina news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,5 +3,12 @@
 @section('title', 'Le ultime notizie')
 
 @section('content')
-    <p>contenuto della news</p>
+    <div class="news-container">
+        <h2>Tutte le nostre news</h2>
+        <div class="box-container">
+            <div class="box">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, veritatis qui. Praesentium, natus. Excepturi dolorum maxime, nisi, est, voluptatem nemo repellendus mollitia molestiae quod ab ipsam doloribus dolor magnam dignissimos.</div>
+            <div class="box">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi culpa nam sint quia delectus dolor magni veniam hic, voluptatum, porro deleniti repudiandae impedit facilis exercitationem tempora mollitia eveniet at deserunt!</div>
+            <div class="box">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae deleniti voluptatibus ratione repellat facilis esse sed reiciendis ullam iure in pariatur officia dolorum aliquam quo, totam cum expedita quidem reprehenderit.</div>
+        </div>
+    </div>
 @endsection
